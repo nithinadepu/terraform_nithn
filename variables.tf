670e3e44bb9a9b7e29a86d480530af4d1379ae38
nithin.adepu <nithin.adepu@gmail.com>
2025-09-29 11:04:58 +0000

    changed variables file

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
  description = "AWS region to deploy infrastructure"
  type = string
- default = "eu-north-1b"
+ default = "eu-north-1"
 }
 variable "instance_type" {
  description = "Instance type for EC2"
